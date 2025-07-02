db27c60168ecb8d816f4d3de8591b61cfd32ec5d
Vlad Vesninsky <111026962+Vlad774@users.noreply.github.com>
2025-07-02 12:56:57 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,3 +11,37 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
+
+
+resource "aws_security_group" "Amelia_n8n_sg" {
+  name        = "Amelia_n8n-sg"
+  description = "Allow HTTP, HTTPS, and n8n ports"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5678
+    to_port     = 5678
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
