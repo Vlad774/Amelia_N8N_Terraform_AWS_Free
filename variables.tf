2fd75c28e8eda07331eee2dae6fafb9eaf5dc149
Vlad Vesninsky <111026962+Vlad774@users.noreply.github.com>
2025-07-02 13:03:00 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,3 +8,8 @@ variable "ami_id" {
   default     = "ami-053b0d53c279acc90" # Ubuntu 22.04 LTS for us-east-1
 }
 
+variable "instance_type" {
+  description = "EC2 instance type"
+  default     = "t3.micro"
+}
+
